a6d466c020d750509683951a591255de42ccc511
Solodky Roman <solodky.dev@gmail.com>
2018-12-19 20:28:07 +0200

    Add files via upload

--- js/frontend.js
+++ js/frontend.js
@@ -2,12 +2,18 @@ var links = document.getElementsByClassName("mobile__menu__link");
 var mobmenu = document.getElementById('mob-menu');
 var closebtn = document.getElementById('close-menu-btn');
 var showbtn = document.getElementById('show-menu-btn');
+
+// show menu
 showbtn.onclick = function() {
   toggleMenu();
 }
+
+// close btn
 closebtn.onclick = function() {
   toggleMenu();
 }
+
+// open/close menu
 function toggleMenu() {
   mobmenu.classList.toggle('active');
   closebtn.classList.toggle('active');
@@ -17,13 +23,36 @@ function toggleMenu() {
     links[i].classList.toggle('removed');
     }
 }
+
+// hide/show header on scroll
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
-  var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
+  var desktopNav = document.querySelector(".mobile__btn");
+  var navDisplayed = getComputedStyle(desktopNav).display;
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+      document.getElementById("navbar").style.transform = "translateY(0)";
+    } else {
+      if (navDisplayed == "flex") {
+        document.getElementById("navbar").style.transform = "translateY(-100%)";
+      }
+    }
+    prevScrollpos = currentScrollPos;
+}
+
+// show header menu on the wide screen in case it has been hidden on the small screen
+window.onresize = function(event) {
+  if (window.innerWidth >= 768) {
     document.getElementById("navbar").style.transform = "translateY(0)";
-  } else {
-    document.getElementById("navbar").style.transform = "translateY(-100%)";
+  };
+}
+
+// scroll into section
+function scrollInto(id) {
+  if (mobmenu.classList.contains('active')) {
+    toggleMenu();
   }
-  prevScrollpos = currentScrollPos;
+  document.getElementById(id).scrollIntoView({
+    behavior: 'smooth',
+  });
 }
